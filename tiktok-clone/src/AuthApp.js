ce94cc77acb9cb8440125d523a469692e5643734
Gary Liggins <GLiggins@kmbsus.local>
2023-01-23 20:00:49 -0800

    adding routing

--- tiktok-clone/src/AuthApp.js
+++ tiktok-clone/src/AuthApp.js
@@ -1,3 +1,32 @@
+import Header from "components/Header";
+import Feed from "pages/Feed";
+import NotFound from "pages/NotFound";
+import Profile from "pages/Profile";
+import Upload from "pages/Upload";
+import VideoPost from "pages/VideoPost";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 export default function AuthApp() {
-  return "authapp";
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Feed/>
+        </Route>
+        <Route exact path='/upload'>
+          <Upload/>
+        </Route>
+        <Route exact path='/:username/video/:postId'>
+          <VideoPost/>
+          </Route>
+          <Route exact path='/:username'>
+          <Profile/>
+        </Route>
+        <Route exact path='*'>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
--- tiktok-clone/src/components/AppProviders.js
+++ tiktok-clone/src/components/AppProviders.js
@@ -1,8 +1,12 @@
+import { UserProvider } from "context/userContext";
 import React from "react";
 import { Toaster } from "react-hot-toast";
 
 export default function AppProviders({ children }) {
-  return <React.StrictMode>{children}
+  return <React.StrictMode>
+  <UserProvider>
+    {children}
   <Toaster/>
+  </UserProvider>
   </React.StrictMode>;
 }
